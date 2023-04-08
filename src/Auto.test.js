4b279829802634b3f69e0882ad39347018ea56c8
KarenAguayo <karen.aguayo@ucb.edu.bo>
2023-04-07 22:31:47 -0400

    PRUEBA  GIRAR DERECHA VARIAS VECES

--- src/Auto.test.js
+++ src/Auto.test.js
@@ -16,4 +16,9 @@ describe("Jugar", () => {
         auto.girar("D");
         expect(auto.getPosicionFinal()).toEqual("0,0E");
     });
+    it("Al enviar DDDD deberia girar cuatro veces a la derecha", () => {
+        const auto = new Auto();
+        auto.girar("DDDD");
+        expect(auto.getPosicionFinal()).toEqual("0,0N");
+    });
   });
