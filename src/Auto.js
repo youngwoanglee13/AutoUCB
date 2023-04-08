8affdff23222a6368f9d28514cc380e85a423e4a
younglee <young.lee@ucb.edu.bo>
2023-04-07 22:02:03 -0400

    REFACTORIZACION IF ANIDADOS

--- src/Auto.js
+++ src/Auto.js
@@ -5,22 +5,24 @@ class Auto{
     girar(cadena){ 
         for(let i = 0; i < cadena.length; i++){
             if(cadena[i] == "I"){
-                if(this.orientacion == "N"){
-                    this.orientacion = "O";
-                }
-                else if(this.orientacion == "O"){
-                    this.orientacion = "S";
-                }
-                else if(this.orientacion == "S"){
-                    this.orientacion = "E";
-                }
-                else if(this.orientacion == "E"){
-                    this.orientacion = "N";
-                }
+                this.orientacion = this.girarIzquierda();
             }
         }
     }
-
+    girarIzquierda(){
+        if(this.orientacion == "N"){
+            return "O";
+        }
+        if(this.orientacion == "O"){
+            return "S";
+        }
+        if(this.orientacion == "S"){
+            return "E";
+        }
+        if(this.orientacion == "E"){
+            return "N";
+        }
+    }
     getPosicionFinal(){
         return "0,0"+this.orientacion;
     }
