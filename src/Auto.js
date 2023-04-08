0da7252ea09b7fa5dda87f1d49e707a4c5eee83e
younglee <young.lee@ucb.edu.bo>
2023-04-07 22:00:10 -0400

    IMPLEMENTAR GIRAR IZQUIERDA VARIAS VECES

--- src/Auto.js
+++ src/Auto.js
@@ -3,10 +3,26 @@ class Auto{
         this.orientacion = "N";
     }
     girar(cadena){ 
-
+        for(let i = 0; i < cadena.length; i++){
+            if(cadena[i] == "I"){
+                if(this.orientacion == "N"){
+                    this.orientacion = "O";
+                }
+                else if(this.orientacion == "O"){
+                    this.orientacion = "S";
+                }
+                else if(this.orientacion == "S"){
+                    this.orientacion = "E";
+                }
+                else if(this.orientacion == "E"){
+                    this.orientacion = "N";
+                }
+            }
+        }
     }
+
     getPosicionFinal(){
-        return "0,0O";
+        return "0,0"+this.orientacion;
     }
 
 
