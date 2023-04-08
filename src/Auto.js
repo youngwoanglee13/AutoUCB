4a540374310dba38ad7f92eea818334c86d49931
younglee <young.lee@ucb.edu.bo>
2023-04-07 21:55:47 -0400

    IMPLEMENTAR GIRAR IZQUIERDA UNA SOLA VEZ

---
+++ src/Auto.js
@@ -0,0 +1,14 @@
+class Auto{
+    constructor(){
+        this.orientacion = "N";
+    }
+    girar(cadena){ 
+
+    }
+    getPosicionFinal(){
+        return "0,0O";
+    }
+
+
+}
+export default Auto;
